a888517062174c19f41fe5935c58eaa7d36ccc4a
Miguel-Gallego-2 <miguel.gallego2@udea.edu.co>
2024-03-29 21:44:30 -0500

    .

---
+++ src/colombiansoccerleague/Round.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package colombiansoccerleague;
+
+/**
+ *
+ * @author migue
+ */
+public class Round {
+    private Team team1;
+    private int goalsTeam1;
+    private Team team2;
+    private int goalsTeam2;
+
+    public Team getTeam1() {
+        return team1;
+    }
+
+    public void setTeam1(Team team1) {
+        this.team1 = team1;
+    }
+
+    public int getGoalsTeam1() {
+        return goalsTeam1;
+    }
+
+    public void setGoalsTeam1(int goalsTeam1) {
+        this.goalsTeam1 = goalsTeam1;
+    }
+
+    public Team getTeam2() {
+        return team2;
+    }
+
+    public void setTeam2(Team team2) {
+        this.team2 = team2;
+    }
+
+    public int getGoalsTeam2() {
+        return goalsTeam2;
+    }
+
+    public void setGoalsTeam2(int goalsTeam2) {
+        this.goalsTeam2 = goalsTeam2;
+    }
+
+    public Round() {
+    }
+    
+}
